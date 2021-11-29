696aabc1ed45209e75b81fd985d92169039b500e
Noah Soubliere <nsoubliere@lcs.on.ca>
2021-11-29 14:26:09 -0500

    Initial Commit

---
+++ VaccinationProof/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VaccinationProof
+//
+//  Created by Noah Alexandre Soubliere on 2021-11-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VaccinationProof/VaccinationProofApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaccinationProofApp.swift
+//  VaccinationProof
+//
+//  Created by Noah Alexandre Soubliere on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct VaccinationProofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
